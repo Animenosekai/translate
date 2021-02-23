b7e583b320257c1dd59beb4008d438049dad0f21
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2021-02-23 22:35:46 +0100

    [add] require safeIO>=1.2

--- setup.py
+++ setup.py
@@ -13,14 +13,13 @@ author_email = "niichannomail@gmail.com",
 url = "https://github.com/Animenosekai/translate",
 download_url = "https://github.com/Animenosekai/translate/archive/v1.3.tar.gz",
 keywords = ['python', 'translate', 'translation', 'google-translate', 'yandex-translate', 'bing-translate', 'reverso', 'transliteration', 'detect-language'],
-install_requires = ['safeIO', 'requests'],
+install_requires = ['safeIO>=1.2', 'requests'],
 classifiers = ['Development Status :: 4 - Beta', 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)', 'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.4', 'Programming Language :: Python :: 3.5', 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: 3.9'],
 long_description = readme_description,
 long_description_content_type = "text/markdown",
 include_package_data=True,
 package_data={
-'translatepy':[
-        'LICENSE'],
+'translatepy':['LICENSE'],
 'data': [
     'data/_alpha2_to_alpha3.json',
     'data/_google_translate_domains.json',
