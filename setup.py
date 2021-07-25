e10956a62501ff6dd45ea7d73bff15274c07a9f3
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2021-07-26 00:57:55 +0200

    [change] Changing some tags in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email = "niichannomail@gmail.com",
     url = "https://github.com/Animenosekai/translate",
     download_url = "https://github.com/Animenosekai/translate/archive/v2.0.tar.gz",
-    keywords = ['python', 'translate', 'translation', 'google-translate', 'yandex-translate', 'bing-translate', 'reverso', 'transliteration', 'detect-language', 'text-to-speech'],
+    keywords = ['python', 'translate', 'translation', 'google-translate', 'yandex-translate', 'bing-translate', 'reverso', 'transliteration', 'detect-language', 'text-to-speech', 'deepl', 'language'],
     install_requires = ['requests', 'beautifulsoup4', 'typing; python_version<"3.5"', 'pyuseragents', 'inquirer'],
     classifiers = ['Development Status :: 5 - Production/Stable', 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)', 'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.2', 'Programming Language :: Python :: 3.3', 'Programming Language :: Python :: 3.4', 'Programming Language :: Python :: 3.5', 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: 3.9'],
     long_description = readme_description,
