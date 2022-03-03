497eef55801c3ce1d1dab1516c50163267f499e0
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2022-03-03 13:56:13 +0100

    [CI] adding fast mode tests

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,10 +1,23 @@
 from translatepy import Translate
 
 
+def alternate(func):
+    def wrapper(self):
+        # first test without fast mode
+        print("testing {}".format(func.__name__))
+        self.translator = Translate()
+        func(self)
+        print("testing {} with fast mode enabled".format(func.__name__))
+        self.translator = Translate(fast=True)
+        return func(self)
+    return wrapper
+
+
 class TestAllTranslators:
     def setup(self):
         self.translator = Translate()
 
+    @alternate
     def test_translate(self):
         translation_args_list = [["What cool weather today!", "fr"],
                                  ["Hello", "Japanese", "en"],
@@ -13,6 +26,7 @@ class TestAllTranslators:
         for args in translation_args_list:
             assert self.translator.translate(*args)
 
+    @alternate
     def test_transliterate(self):
         transliteration_args_list = [["What cool weather today!", "ar"],
                                      ["Hello", "Japanese", "en"],
@@ -21,6 +35,7 @@ class TestAllTranslators:
         for args in transliteration_args_list:
             assert self.translator.transliterate(*args)
 
+    @alternate
     def test_spellcheck(self):
         spellcheck_args_list = [["What cool weater todai!"], ["Helo"],
                                 ["Helo, how are tou?"]]
@@ -28,6 +43,7 @@ class TestAllTranslators:
         for args in spellcheck_args_list:
             assert self.translator.spellcheck(*args)
 
+    @alternate
     def test_example(self):
         example_args_list = [["What cool weater todai!", "fr"], ["Helo", "French"],
                              ["Helo, how are tou?", "ru"]]
@@ -35,6 +51,7 @@ class TestAllTranslators:
         for args in example_args_list:
             assert self.translator.example(*args)
 
+    @alternate
     def test_dictionary(self):
         dictionary_args_list = [["What cool weater todai!", "fr"], ["Helo", "French"],
                                 ["Helo, how are tou?", "ru"]]
@@ -42,6 +59,7 @@ class TestAllTranslators:
         for args in dictionary_args_list:
             assert self.translator.dictionary(*args)
 
+    @alternate
     def test_language(self):
         language_args_list = [["What cool weater todaiy"], ["Привет"],
                               ["自动"]]
@@ -49,6 +67,7 @@ class TestAllTranslators:
         for args in language_args_list:
             assert self.translator.language(*args)
 
+    @alternate
     def test_text_to_speech(self):
         texts_args_list = [["What cool weater todaiy"], ["Привет"],
                            ["自动"]]
@@ -56,6 +75,7 @@ class TestAllTranslators:
         for args in texts_args_list:
             assert self.translator.text_to_speech(*args)
 
+    @alternate
     def test_translate_html(self):
         translation_args_list = [("<h1>Hello</h1> <span class='everyone'>everyone</span><br>I hope that <span class='everyone'>everyone</span><div class='okay-container'> is doing <strong>Okay〜!</strong></div>", "ja")]
 
