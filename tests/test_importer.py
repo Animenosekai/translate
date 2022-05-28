8206019733980f31c159a1635180e31d17ccbfad
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2022-05-28 23:38:01 +0200

    [CI] adding tests for importer

---
+++ tests/test_importer.py
@@ -0,0 +1,16 @@
+from translatepy.exceptions import UnknownTranslator
+from translatepy.translators import GoogleTranslate, YandexTranslate
+from translatepy.utils.importer import get_translator
+
+
+def test_importer():
+    print("[test] --> Testing translatepy.utils.importer")
+    assert get_translator("GoogleTranslate") == GoogleTranslate
+    assert get_translator("グーグル") == GoogleTranslate
+    assert get_translator("yandex") == YandexTranslate
+    assert get_translator("translatepy.translators.google.GoogleTranslate") == GoogleTranslate
+    assert get_translator("translatepy.translators.yandex.YandexTranslate") == YandexTranslate
+    try:
+        get_translator("AAAAAAAAA")
+    except UnknownTranslator:
+        pass
