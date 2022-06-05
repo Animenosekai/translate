18616a473d315a8ff17d3312ba4a6e92c0352706
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2022-06-05 23:22:45 +0200

    [update] using correct server domain [update] using Heroku port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python translatepy/__main__.py server
+web: python translatepy/__main__.py server --host "0.0.0.0" --port $PORT
--- website/config.ts
+++ website/config.ts
@@ -1,6 +1,6 @@
 const Configuration = {
     request: {
-        host: process.env.NODE_ENV ? "http://127.0.0.1:5000" : "https://translate.herokuapp.com"
+        host: process.env.NODE_ENV ? "http://127.0.0.1:5000" : "https://anise-translate.herokuapp.com"
     }
 }
 
