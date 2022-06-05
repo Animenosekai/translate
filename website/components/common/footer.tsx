6e0c0de493c6fbd8d706eade51f8a21a63c18a30
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2022-06-06 00:39:01 +0200

    [update] changing favicon

--- website/components/common/footer.tsx
+++ website/components/common/footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
         <div className="space-x-8 flex flex-row">
             <a className="opacity-40 text-black hover:opacity-70 transition" target="_blank" rel="noreferrer" href="https://github.com/Animenosekai/translate">GitHub</a>
             <div className="flex flex-row opacity-40 hover:opacity-70 transition cursor-pointer">
-                <select value={strings.language} className="appearance-none bg-transparent outline-none pr-5 z-[1] cursor-pointer" name="language" onChange={ev => setLanguage(ev.target.value)}>
+                <select value={strings.language} className="appearance-none bg-transparent outline-none pr-6 z-[1] cursor-pointer" name="language" onChange={ev => setLanguage(ev.target.value)}>
                     {
                         Object.keys(localizations).map((lang, key) => {
                             return <option value={lang} key={key}>{(new localizations[lang]()).name}</option>
