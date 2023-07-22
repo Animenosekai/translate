d44a716fce88c45bfc361d84a3ffcfc94cacf0a2
ZhymabekRoman <robanokssamit@yandex.ru>
2023-07-22 23:33:15 +0600

    add: New two Microsoft Translate endpoints

--- translatepy/translators/microsoft.py
+++ translatepy/translators/microsoft.py
@@ -1,12 +1,16 @@
 """
 This implementation was made specifically for translatepy by 'Zhymabek Roman'.
 """
+from icecream import ic
 
-import json
-import re
+import base64
+import hashlib
 import os
 import uuid
+import urllib
+import hmac
 import time
+import datetime as dt
 from safeIO import JSONFile
 
 from translatepy.exceptions import UnsupportedMethod
@@ -73,72 +77,102 @@ class MicrosoftSessionManager():
 
             return response
 
-class MicrosoftTranslate(BaseTranslator):
+class MicrosoftTranslateV2(BaseTranslator):
     """
-    A Python implementation of Microsoft Translation's APIs
+    A Python implementation of Microsoft Translation, reverse engenered from Microsoft Translator Android application.
+    Ported from https://github.com/d4n3436/GTranslate/blob/master/src/GTranslate/Translators/MicrosoftTranslator.cs
+    Huge thanks to d4n3436.
+    """
+
+    _api_endpoint = "dev.microsofttranslator-int.com"
+    _api_version = "3.0"
+    _api_private_key = bytes([
+        0xa2, 0x29, 0x3a, 0x3d, 0xd0, 0xdd, 0x32, 0x73,
+        0x97, 0x7a, 0x64, 0xdb, 0xc2, 0xf3, 0x27, 0xf5,
+        0xd7, 0xbf, 0x87, 0xd9, 0x45, 0x9d, 0xf0, 0x5a,
+        0x09, 0x66, 0xc6, 0x30, 0xc6, 0x6a, 0xaa, 0x84,
+        0x9a, 0x41, 0xaa, 0x94, 0x3a, 0xa8, 0xd5, 0x1a,
+        0x6e, 0x4d, 0xaa, 0xc9, 0xa3, 0x70, 0x12, 0x35,
+        0xc7, 0xeb, 0x12, 0xf6, 0xe8, 0x23, 0x07, 0x9e,
+        0x47, 0x10, 0x95, 0x91, 0x88, 0x55, 0xd8, 0x17
+    ])
+
+    def __init__(self, request: Request = Request()):
+        self.session = request
+
+    def _translate(self, text, destination_language, source_language):
+        translate_url = f"{self._api_endpoint}/translate?api-version={self._api_version}&to={destination_language}"
+        ic(translate_url)
+        if source_language != "auto":
+            translate_url += "&from={source_language}"
+
+        request = self.session.post(f"https://{translate_url}", headers={"X-MT-Signature": self._get_signature(translate_url)}, json=[{"text": text}])
+        response = request.json()
+        ic(response)
+        return response
+
+    def _get_signature(self, url):
+        guid = uuid.uuid4().hex
+        escaped_url = urllib.parse.quote(url, safe='')
+
+        now = dt.datetime.utcnow()
+        date_str = now.strftime("%a, %d %b %Y %H:%M:%S GMT").lower()
+
+        data_to_sign = f"MSTranslatorAndroidApp{escaped_url}{date_str}{guid}".lower().encode('utf-8')
+
+        hash_bytes = hmac.new(self._api_private_key, data_to_sign, hashlib.sha256).digest()
+        signature = f"MSTranslatorAndroidApp::{base64.b64encode(hash_bytes).decode()}::{date_str}::{guid}"
+        ic(signature)
+        print(signature)
+        return signature
+
+    def _language_normalize(self, language):
+        _language = Language(language)
+        if language.id == "zho":
+            return "zh-Hans"
+        elif language.id == "och":
+            return "zh-Hant"
+        return _language.alpha2
+
+    def _language_denormalize(self, language_code):
+        if str(language_code).lower() in {"zh-cn", "zh-hans"}:
+            return Language("zho")
+        elif str(language_code).lower() == "zh-tw":
+            return Language("och")
+        return Language(language_code)
+
+
+class MicrosoftTranslateV1(BaseTranslator):
+    """
+    A Python implementation of Microsoft Translation, reverse engenered from Microsoft SwiftKey
+
+    Also I found '/v1/languages?scope=translation' endpoint, idk maybe it can be useful
     """
 
     _supported_languages = {'auto', 'af', 'sq', 'am', 'ar', 'hy', 'as', 'az', 'bn', 'bs', 'bg', 'my', 'ca', 'ca', 'zh-Hans', 'cs', 'da', 'nl', 'nl', 'en', 'et', 'fj', 'fil', 'fil', 'fi', 'fr', 'fr-ca', 'de', 'ga', 'el', 'gu', 'ht', 'ht', 'he', 'hi', 'hr', 'hu', 'is', 'iu', 'id', 'it', 'ja', 'kn', 'kk', 'km', 'ko', 'ku', 'lo', 'lv', 'lt', 'ml', 'mi', 'mr', 'ms', 'mg', 'mt', 'ne', 'nb', 'nb', 'or', 'pa', 'pa', 'fa', 'pl', 'pt', 'ps', 'ps', 'ro', 'ro', 'ro', 'ru', 'sk', 'sl', 'sm', 'es', 'es', 'sr-Cyrl', 'sw', 'sv', 'ty', 'ta', 'te', 'th', 'ti', 'tlh-Latn', 'tlh-Latn', 'to', 'tr', 'uk', 'ur', 'vi', 'cy', 'zh-Hans', 'zh-Hant', 'yue', 'prs', 'mww', 'tlh-Piqd', 'kmr', 'pt-pt', 'otq', 'sr-Cyrl', 'sr-Latn', 'yua'}
 
     def __init__(self, request: Request = Request()):
-        self.session_manager = MicrosoftSessionManager(request)
         self.session = request
+        self.session.headers["Authorization"] = "Bearer 16318c3a-5fb1-4091-8f63-65aa993e2f1d"
+        self._update_trace_id()
+
+    def _update_trace_id(self):
+        self.session.headers["X-ClientTraceId"] = str(uuid.uuid4())
 
     def _translate(self, text: str, destination_language: str, source_language: str) -> str:
         if source_language == "auto":
             source_language = self._language(text)
 
-        response = self.session_manager.send("https://api.cognitive.microsofttranslator.com/translate", params={'from': source_language, 'to': destination_language}, data=[{"text": text}])
+        request = self.session.post("https://translate.api.swiftkey.com/v1/translate", params={'from': source_language, 'to': destination_language}, json=[{"text": text}])
+        response = request.json()
+        self._update_trace_id()
         return source_language, response[0]["translations"][0]["text"]
 
-    def _example(self, text, destination_language, source_language) -> str:
-        source_language, translation = self._translate(text, destination_language, source_language)
-
-        response = self.session_manager.send("https://api.cognitive.microsofttranslator.com/dictionary/examples", params={'from': source_language, 'to': destination_language}, data=[{'Text': text.lower(), 'Translation': translation.lower()}])
-        return source_language, [BingExampleResult(example) for example in response[0]["examples"]]
-
     def _language(self, text: str) -> str:
-        response = self.session_manager.send("https://api.cognitive.microsofttranslator.com/detect", data=[{"text": text}])
-        return response[0]["language"]
-
-    # def _transliterate(self, text: str, destination_language: str, source_language: str):
-        # TODO: Implement
-
-    def _dictionary(self, text: str, destination_language: str, source_language: str):
-        source_language = self._language(text)
-
-        response = self.session_manager.send("https://api.cognitive.microsofttranslator.com/dictionary/lookup", data=[{'text': text}], params={'from': source_language, 'to': destination_language})
-        _result = []
-        for _dictionary in response[0]["translations"]:
-            _dictionary_result = _dictionary["displayTarget"]
-            _result.append(_dictionary_result)
-        return source_language, _result
-
-    def _text_to_speech(self, text: str, speed: int, gender: str, source_language: str):
-        if source_language == "auto":
-            source_language = self._language(text)
-
-        gender = gender.capitalize()
-
-        _supported_langs_url = "https://{region}.tts.speech.microsoft.com/cognitiveservices/voices/list".format(region=self.session_manager._region)
-        _supported_langs_list = self.session_manager.send(_supported_langs_url)
-
-        # all locals list: {('zh-HK', 'zh-HK'), ('de', 'de-DE'), ('da', 'da-DK'), ('id', 'id-ID'), ('ko', 'ko-KR'), ('en', 'en-NZ'), ('el', 'el-GR'), ('ms', 'ms-MY'), ('es', 'es-AR'), ('ro', 'ro-RO'), ('pl', 'pl-PL'), ('it', 'it-IT'), ('hr', 'hr-HR'), ('pt', 'pt-PT'), ('hu', 'hu-HU'), ('sw', 'sw-KE'), ('en', 'en-GB'), ('mt', 'mt-MT'), ('tr', 'tr-TR'), ('ar', 'ar-EG'), ('fr', 'fr-CA'), ('te', 'te-IN'), ('fr', 'fr-BE'), ('en', 'en-SG'), ('zh-CN', 'zh-CN'), ('fr', 'fr-FR'), ('en', 'en-PH'), ('cs', 'cs-CZ'), ('fi', 'fi-FI'), ('zh-TW', 'zh-TW'), ('de', 'de-CH'), ('nb', 'nb-NO'), ('bg', 'bg-BG'), ('he', 'he-IL'), ('en', 'en-CA'), ('en', 'en-HK'), ('es', 'es-MX'), ('en', 'en-AU'), ('th', 'th-TH'), ('pt', 'pt-BR'), ('mr', 'mr-IN'), ('sk', 'sk-SK'), ('ru', 'ru-RU'), ('nl', 'nl-NL'), ('en', 'en-US'), ('ta', 'ta-IN'), ('hi', 'hi-IN'), ('cy', 'cy-GB'), ('ar', 'ar-SA'), ('ga', 'ga-IE'), ('nl', 'nl-BE'), ('de', 'de-AT'), ('ca', 'ca-ES'), ('uk', 'uk-UA'), ('es', 'es-CO'), ('es', 'es-ES'), ('es', 'es-US'), ('en', 'en-ZA'), ('ur', 'ur-PK'), ('sv', 'sv-SE'), ('lv', 'lv-LV'), ('lt', 'lt-LT'), ('vi', 'vi-VN'), ('et', 'et-EE'), ('en', 'en-IN'), ('en', 'en-IE'), ('ja', 'ja-JP'), ('fr', 'fr-CH'), ('gu', 'gu-IN'), ('sl', 'sl-SI')}
-        _locals = {'zh-CN': 'zh-CN', 'mr': 'mr-IN', 'en': 'en-US', 'ru': 'ru-RU', 'el': 'el-GR', 'es': 'es-CO', 'id': 'id-ID', 'pt': 'pt-PT', 'ko': 'ko-KR', 'ta': 'ta-IN', 'te': 'te-IN', 'et': 'et-EE', 'pl': 'pl-PL', 'it': 'it-IT', 'ms': 'ms-MY', 'mt': 'mt-MT', 'ro': 'ro-RO', 'vi': 'vi-VN', 'bg': 'bg-BG', 'zh-TW': 'zh-TW', 'tr': 'tr-TR', 'de': 'de-CH', 'fr': 'fr-CH', 'nb': 'nb-NO', 'nl': 'nl-BE', 'uk': 'uk-UA', 'he': 'he-IL', 'ur': 'ur-PK', 'hi': 'hi-IN', 'ja': 'ja-JP', 'hr': 'hr-HR', 'sv': 'sv-SE', 'hu': 'hu-HU', 'sw': 'sw-KE', 'lt': 'lt-LT', 'sl': 'sl-SI', 'fi': 'fi-FI', 'lv': 'lv-LV', 'sk': 'sk-SK', 'da': 'da-DK', 'cy': 'cy-GB', 'gu': 'gu-IN', 'ga': 'ga-IE', 'th': 'th-TH', 'ar': 'ar-EG', 'ca': 'ca-ES', 'zh-HK': 'zh-HK', 'cs': 'cs-CZ'}
-        _source_local = _locals.get(source_language)
-
-        for _supported_lang in _supported_langs_list:
-            if _supported_lang["Locale"] == _source_local and _supported_lang["Gender"] == gender:
-                voice = _supported_lang["ShortName"]
-                break
-        else:
-            raise UnsupportedMethod("Microsoft Translate doesn't support {source_lang} language".format(source_lang=source_language))
-
-        speech_url = "https://{region}.tts.speech.microsoft.com/cognitiveservices/v1".format(region=self.session_manager._region)
-        headers = {"authorization": "Bearer {token}".format(token=self.session_manager._token), "content-type": "application/ssml+xml", "x-microsoft-outputformat": "audio-48khz-192kbitrate-mono-mp3"}
-        data = "<speak version='1.0' xml:lang='{local}'><voice xml:lang='{local}' xml:gender='{gender}' name='{voice}'><prosody rate='{speed}%'>{text}</prosody></voice></speak>".format(text=text, gender=gender, speed=float(speed - 100), local=_source_local, voice=voice)
-        spech_result = self.session.post(speech_url, data=data.encode('utf-8'), headers=headers)
-        return source_language, spech_result.content
+        request = self.session.post("https://translate.api.swiftkey.com/v1/translate", params={'to': "en"}, json=[{"text": text}])
+        response = request.json()
+        self._update_trace_id()
+        return response[0]["detectedLanguage"]["language"]
 
     def _language_normalize(self, language):
         _language = Language(language)
@@ -156,4 +190,6 @@ class MicrosoftTranslate(BaseTranslator):
         return Language(language_code)
 
     def __str__(self) -> str:
-        return "Microsoft Translate"
+        return "Microsoft Translate V1"
+
+MicrosoftTranslate = MicrosoftTranslateV1
