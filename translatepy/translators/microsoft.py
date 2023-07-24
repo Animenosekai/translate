13886aefe5405f3825b34de59c52b50c80f4cd7e
ZhymabekRoman <robanokssamit@yandex.ru>
2023-07-24 21:55:53 +0600

    fix: working MicrosoftTranslateV2 huray!

--- translatepy/translators/microsoft.py
+++ translatepy/translators/microsoft.py
@@ -1,81 +1,19 @@
 """
 This implementation was made specifically for translatepy by 'Zhymabek Roman'.
 """
-from icecream import ic
-
 import base64
 import hashlib
-import os
 import uuid
 import urllib
 import hmac
-import time
 import datetime as dt
-from safeIO import JSONFile
 
 from translatepy.exceptions import UnsupportedMethod
 from translatepy.language import Language
 from translatepy.translators.base import BaseTranslateException, BaseTranslator
 from translatepy.utils.request import Request
-from translatepy.utils.annotations import Callable, Dict
-from translatepy.translators.bing import BingSessionManager, BingExampleResult
-
-HOME_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class MicrosoftException(BaseTranslateException):
-    error_codes = {
-        429: "Too many requests"
-    }
-
-
-class MicrosoftSessionManager():
-    def __init__(self, request: Request):
-        self.session = request
-        self.bing_session = BingSessionManager(request)
-
-        self._auth_session_file = JSONFile(os.path.join(HOME_DIR, ".microsoft_translatepy"), blocking=False)
-        with self._auth_session_file as _auth_session:
-            _auth_session_data = _auth_session.read()
-        self._region, self._token, self._token_expiries = _auth_session_data.get("region"), _auth_session_data.get("token"), _auth_session_data.get("token_expiries", 0)
-        self._parse_authorization_data()
-
-    def _parse_authorization_data(self, force: bool = False):
-        if not self._token or time.time() > self._token_expiries or force:
-            # authentication token is valid for 10 minutes
-            token_response = self.bing_session.send("https://www.bing.com/tfetspktok", data={})
-            token_status = token_response.get("statusCode", 200)
-
-            if token_status != 200:
-                raise MicrosoftException(token_status, "Error during token request from the server")
-
-            self._token, self._region = token_response.get("token"), token_response.get("region")
-            self._token_expiries = time.time() + (int(token_response.get("expiryDurationInMS", 600000)) - 1000) / 1000
-
-            self._auth_session_file.write({"token": self._token, "region": self._region, "token_expiries": self._token_expiries})
-
-    def send(self, url, data, params: Dict = {}):
-        # Try 2 times to make a request
-        for _ in range(2):
-            headers = {
-                'Authorization': 'Bearer {token}'.format(token=self._token),
-                'Content-type': 'application/json',
-                'X-ClientTraceId': str(uuid.uuid4())
-            }
-            _params = {'api-version': '3.0'}
-            _params.update(params)
-
-            request = self.session.post(url, params=_params, json=data, headers=headers)
-            response = request.json()
-
-            if request.status_code != 200:
-                error = response.get("error", {})
-                if error.get("code", request.status_code) == 401000:
-                    self._parse_authorization_data(force=True)
-                    continue
-                raise MicrosoftException(status_code=error.get("code"), message=error.get("message", "Unknown"))
-
-            return response
 
 class MicrosoftTranslateV2(BaseTranslator):
     """
@@ -102,28 +40,28 @@ class MicrosoftTranslateV2(BaseTranslator):
 
     def _translate(self, text, destination_language, source_language):
         translate_url = f"{self._api_endpoint}/translate?api-version={self._api_version}&to={destination_language}"
-        ic(translate_url)
         if source_language != "auto":
-            translate_url += "&from={source_language}"
+            translate_url += f"&from={source_language}"
 
         request = self.session.post(f"https://{translate_url}", headers={"X-MT-Signature": self._get_signature(translate_url)}, json=[{"text": text}])
         response = request.json()
-        ic(response)
-        return response
+
+        if source_language == "auto":
+            source_language = response[0]["detectedLanguage"]["language"]
+
+        return source_language, response[0]["translations"][0]["text"]
 
     def _get_signature(self, url):
         guid = uuid.uuid4().hex
-        escaped_url = urllib.parse.quote(url, safe='')
+        escaped_url = urllib.parse.quote_plus(url)
 
         now = dt.datetime.utcnow()
-        date_str = now.strftime("%a, %d %b %Y %H:%M:%S GMT").lower()
+        date_str = now.strftime("%a, %d %b %Y %H:%M:%SGMT")
 
         data_to_sign = f"MSTranslatorAndroidApp{escaped_url}{date_str}{guid}".lower().encode('utf-8')
 
         hash_bytes = hmac.new(self._api_private_key, data_to_sign, hashlib.sha256).digest()
         signature = f"MSTranslatorAndroidApp::{base64.b64encode(hash_bytes).decode()}::{date_str}::{guid}"
-        ic(signature)
-        print(signature)
         return signature
 
     def _language_normalize(self, language):
@@ -154,10 +92,6 @@ class MicrosoftTranslateV1(BaseTranslator):
     def __init__(self, request: Request = Request()):
         self.session = request
         self.session.headers["Authorization"] = "Bearer 16318c3a-5fb1-4091-8f63-65aa993e2f1d"
-        self._update_trace_id()
-
-    def _update_trace_id(self):
-        self.session.headers["X-ClientTraceId"] = str(uuid.uuid4())
 
     def _translate(self, text: str, destination_language: str, source_language: str) -> str:
         if source_language == "auto":
@@ -165,13 +99,11 @@ class MicrosoftTranslateV1(BaseTranslator):
 
         request = self.session.post("https://translate.api.swiftkey.com/v1/translate", params={'from': source_language, 'to': destination_language}, json=[{"text": text}])
         response = request.json()
-        self._update_trace_id()
         return source_language, response[0]["translations"][0]["text"]
 
     def _language(self, text: str) -> str:
         request = self.session.post("https://translate.api.swiftkey.com/v1/translate", params={'to': "en"}, json=[{"text": text}])
         response = request.json()
-        self._update_trace_id()
         return response[0]["detectedLanguage"]["language"]
 
     def _language_normalize(self, language):
