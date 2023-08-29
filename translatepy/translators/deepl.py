81768f67ddb4a28b2024468ccf3a7bdd051dff61
ZhymabekRoman <robanokssamit@yandex.ru>
2023-08-29 17:05:44 +0600

    deepl: new implementation based on Android version

--- translatepy/translators/deepl.py
+++ translatepy/translators/deepl.py
@@ -14,7 +14,9 @@ ZhymabekRoman
 
 import re
 import time
+import uuid
 import random
+import secrets
 import typing
 
 from bs4 import BeautifulSoup
@@ -98,22 +100,24 @@ class JSONRPCRequest:
             distance = 3 - (time.time() - self.last_access)
             time.sleep((distance if distance >= 0 else 0))
 
-        request = self.session.post("https://www2.deepl.com/jsonrpc", json=self.dump(method, params))
+        request = self.session.post("https://www2.deepl.com/jsonrpc", json={"method": method, "params": params}, params={"client": "chrome-extension,1.6.1"})
         self.last_access = time.time()
         response = request.json()
         if request.status_code == 200:
             return response["result"]
         else:
-            raise DeeplTranslateException(response["error"]["code"])
+            raise DeeplTranslateException(response["error"]["code"], response["error"]["message"])
 
 
-class DeeplTranslate(BaseTranslator):
+class DeeplTranslateV1(BaseTranslator):
 
     _supported_languages = {'AUTO', 'BG', 'CS', 'DA', 'DE', 'EL', 'EN', 'ES', 'ET', 'FI', 'FR', 'HU', 'IT', 'JA', 'LT', 'LV', 'NL', 'PL', 'PT', 'RO', 'RU', 'SK', 'SL', 'SV', 'ZH', 'TR', 'ID', 'UK'}
 
     def __init__(self, session: request.Session = None, preferred_langs: typing.List = ["EN", "RU"]) -> None:
         super().__init__(session)
-        self.jsonrpc = JSONRPCRequest(session)
+        self.session.headers["User-Agent"] = "DeepLBrowserExtension/1.6.1 Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+        self.session.get("https://static.deepl.com/img/illustrations/pro-features-3x1.svg")  # we need to get some security cookies from Cloudflare
+        self.jsonrpc = JSONRPCRequest(self.session)
         self.user_preferred_langs = preferred_langs
 
     def _split_into_sentences(self, text: str, dest_lang: str, source_lang: str) -> typing.Tuple[typing.List[str], str]:
@@ -132,7 +136,7 @@ class DeeplTranslate(BaseTranslator):
             "texts": [text.strip()],  # What for need strip there?
             "lang": {
                 "lang_user_selected": source_lang,
-                "user_preferred_langs": typing.List(set(self.user_preferred_langs + [dest_lang]))
+                "user_preferred_langs": list(set(self.user_preferred_langs + [dest_lang]))
             }
         }
         resp = self.jsonrpc.send_jsonrpc("LMT_split_into_sentences", params)
@@ -290,4 +294,81 @@ class DeeplTranslate(BaseTranslator):
         return Language(code)
 
     def __str__(self) -> str:
-        return "DeepL"
+        return "DeepL Web"
+
+
+
+class DeeplTranslateV2(BaseTranslator):
+    _client_id = "f02c852d-109d-448c-a9fb-5a805000f8cb"
+    _device_name = secrets.token_hex(16)
+
+    def __init__(self, session: request.Session = None) -> None:
+        super().__init__(session)
+        self.session.headers["User-Agent"] = f"DeepL/2.4(69) Android 11 ({self._device_name};aarch64)"
+        self.id_number = (random.randint(1000, 9999) * 10000) + 1  # ? I didn't verify the range, but it's better having only DeepL not working than having Translator() crash for only one service
+
+    def _translate(self, text, dest_lang, source_lang):
+        timestamp = int(time.time() * 10) * 100 + 1000
+
+        trace_id = str(uuid.uuid4()).replace("-", "")
+
+        random_bytes = secrets.token_bytes(8)
+        random_hex = secrets.token_hex(len(random_bytes))
+
+        headers = {
+            "referer": "https://www.deepl.com/",
+            "traceparent": f"00-{trace_id}-{random_hex}-01",
+            "x-trace-id": trace_id,
+            "x-instance": self._client_id,
+            "client-id": self._client_id,
+            "x-app-os-name": "Android",
+            "x-app-os-version": "11",
+            "x-app-version": "2.4",
+            "x-app-build": "69",
+            "x-app-device": self._device_name,
+            "x-app-instance-id": self._client_id,
+            "accept-encoding": "gzip"
+        }
+
+        data = {
+            "params": {
+                "texts": [{"text": text, "requestAlternatives": 3}],
+                "splitting": "newlines",
+                "commonJobParams": {"wasSpoken": False},
+                "lang": {"target_lang": dest_lang, "source_lang_user_selected": source_lang},
+                "timestamp": timestamp
+            },
+            "id": self.id_number,
+            "jsonrpc": "2.0",
+            "method": "LMT_handle_texts"
+        }
+
+        request = self.session.post("https://www2.deepl.com/jsonrpc", headers=headers, json=data)
+
+        if request.status_code != 200:
+            raise DeeplTranslateException(request.status_code, request.text)
+
+        response = request.json()
+
+        self.id_number += 1
+
+        source_lang = response["result"]["lang"]
+        dest_lang = list(response["result"]["detectedLanguages"])[0]
+
+        return models.TranslationResult(source_lang=source_lang, dest_lang=dest_lang, translation=response["result"]["texts"][0]["text"])
+
+    def _language_to_code(self, language: Language) -> typing.Union[str, typing.Any]:
+        if language.id == "zho":
+            return "ZH"
+        elif language.id == "auto":
+            return ""
+        return language.alpha2.upper()
+
+    def _code_to_language(self, code: typing.Union[str, typing.Any]) -> Language:
+        if str(code).lower() in {"zh", "zh-cn"}:
+            return Language("zho")
+        elif str(code) == "":
+            return Language("auto")
+        return Language(code)
+
+DeeplTranslate = DeeplTranslateV1
