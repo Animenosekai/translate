3f537e2cb98c92d5d13f80d49f3ca81a9b16a2d9
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2023-09-03 18:05:33 +0200

    [update] structuring the build script

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-from translatepy.translators.papago import Papago
-from rich.console import Console
+import enum
 
-console = Console()
 
-try:
-    p = Papago()
-    # t = p.translate("Hello world", "Japanese")
-    # print(t.__pretty__(cli=True))
-    r = p.text_to_speech("こんにちは", source_lang="Japanese")
-    r.write_to_file(f"hello.{r.extension}")
-except Exception:
-    console.print_exception(show_locals=True)
+class Test(enum.Enum):
+    TEST = "test"
+    BUILD = "build"
+
+
+print(Test(1))
